54e69502655064e6e2a392ef81e7ca240557772c
Валерий Кравчук <valery.v.kravchuk@gmail.com>
2021-10-13 21:36:32 +0300

    Task GeometryCalculator completed

--- 07_NumbersAndDates/GeometryCalculator/src/main/java/GeometryCalculator.java
+++ 07_NumbersAndDates/GeometryCalculator/src/main/java/GeometryCalculator.java
@@ -10,7 +10,7 @@ public class GeometryCalculator {
     }
 
     public static boolean isTrianglePossible(double a, double b, double c) {
-        return (a + b > c && a + c > b && b + c > a ? true : false);
+        return (a + b > c && a + c > b && b + c > a);
     }
 
     // перед расчетом площади рекомендуется проверить возможен ли такой треугольник
