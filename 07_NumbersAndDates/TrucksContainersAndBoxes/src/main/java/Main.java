037e376cd3f81fea48366b3310e17129816d4bb3
Валерий Кравчук <valery.v.kravchuk@gmail.com>
2021-10-15 16:06:18 +0300

    Task TrucksContainersAndBoxes completed

--- 07_NumbersAndDates/TrucksContainersAndBoxes/src/main/java/Main.java
+++ 07_NumbersAndDates/TrucksContainersAndBoxes/src/main/java/Main.java
@@ -6,19 +6,27 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String boxes = scanner.nextLine();
 
-        // TODO: вывести в консоль коробки разложенные по грузовикам и контейнерам
-        // пример вывода при вводе 2
-        // для отступа используйте табуляцию - \t
+        final int BOXES_IN_CONTAINER = 27;
+        final int CONTAINERS_IN_TRACK = 12;
 
-        /*
-        Грузовик: 1
-            Контейнер: 1
-                Ящик: 1
-                Ящик: 2
-        Необходимо:
-        грузовиков - 1 шт.
-        контейнеров - 1 шт.
-        */
+        Integer boxesCounter;
+        boxesCounter = Integer.parseInt(boxes);
+
+        int containerCounter = 0;
+        int tracksCounter = 0;
+
+        for (int i = 0; i < boxesCounter; i++) {
+            if (i % BOXES_IN_CONTAINER == 0) {
+                if (containerCounter % CONTAINERS_IN_TRACK == 0) {
+                    System.out.println("Грузовик: " + ++tracksCounter);
+                }
+                System.out.println("\tКонтейнер: " + ++containerCounter);
+            }
+            System.out.println("\t\tЯщик: " + (i + 1));
+        }
+
+        System.out.println("Необходимо:");
+        System.out.println("грузовиков - " + tracksCounter + " шт.");
+        System.out.println("контейнеров - " + containerCounter + " шт.");
     }
-
 }
