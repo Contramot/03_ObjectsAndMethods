521ca6aae64ebf0e8e85b1882e2ee28342d2b514
Валерий Кравчук <valery.v.kravchuk@gmail.com>
2021-10-18 18:16:28 +0300

    Task Periods completed

--- 07_NumbersAndDates/Periods/src/main/java/Main.java
+++ 07_NumbersAndDates/Periods/src/main/java/Main.java
@@ -1,13 +1,27 @@
 import java.time.LocalDate;
+import java.time.Period;
 
 public class Main {
 
   public static void main(String[] args) {
 
+    LocalDate javaBirthdayDate = LocalDate.of(1996, 1, 26);
+
+    System.out.println(getPeriodFromBirthday(javaBirthdayDate));
+
   }
 
   private static String getPeriodFromBirthday(LocalDate birthday) {
-    return "";
+
+    LocalDate today = LocalDate.now();
+    Period javaAge = Period.between(birthday, today);
+
+    String periodFromBirthday = "";
+    periodFromBirthday += javaAge.getYears() + " years, " +
+            javaAge.getMonths() + " months, " +
+            javaAge.getDays() + " days";
+
+    return periodFromBirthday;
   }
 
 }
