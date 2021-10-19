77dc0c6c1d6c60a5cade1f99547cd22982ffa1cc
Валерий Кравчук <valery.v.kravchuk@gmail.com>
2021-10-19 19:17:16 +0300

    Task TotalEarningsCalculator completed

--- 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
+++ 08_Strings/TotalEarningsCalculator/src/main/java/Main.java
@@ -3,7 +3,35 @@ public class Main {
   public static void main(String[] args) {
 
     String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-    //TODO: напишите ваш код, результат вывести в консоль
+
+    System.out.println(calculationTotalIncome(text));
   }
 
+  public static int calculationTotalIncome (String text) {
+    int totalIncome = 0;
+    int start = 0, stop = 0;
+
+    for (int i = 0; i < text.length(); i++) {
+      char symbol = text.charAt(i);
+      if (symbol >= '0' && symbol <= '9') {
+        start = i;
+
+        for (int j = ++i; j < text.length(); j++) {
+          symbol = text.charAt(j);
+          if (symbol >= '0' && symbol <= '9') {
+            stop = j;
+          } else {
+            String encodingSum = text.substring(start, stop + 1);
+            int sum = Integer.parseInt(encodingSum);
+            totalIncome += sum;
+            i = ++j;
+            break;
+          }
+        }
+
+      }
+    }
+
+    return totalIncome;
+  }
 }
