be0d14bbdcef7b4d4e561fae3b343ef818fd2453
Валерий Кравчук <valery.v.kravchuk@gmail.com>
2021-10-21 20:54:15 +0300

    Task FullNameFormatter completed

--- 08_Strings/FullNameFormatter/src/main/java/Main.java
+++ 08_Strings/FullNameFormatter/src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -10,9 +11,45 @@ public class Main {
       if (input.equals("0")) {
         break;
       }
-      //TODO:напишите ваш код тут, результат вывести в консоль.
+      //напишите ваш код тут, результат вывести в консоль.
       //При невалидном ФИО вывести в консоль: Введенная строка не является ФИО
+
+      System.out.println((checkingStringForValidity(input) ? formattingFullName(input) : "Введенная строка не является ФИО"));
     }
+
   }
 
+  public static boolean checkingStringForValidity (String fullName) {
+    boolean result = false;
+
+    for (int i = 0; i < fullName.length(); i++) {
+      char symbol = fullName.charAt(i);
+      if ((symbol >= 'А' && symbol <= 'я') || symbol == 'Ё' || symbol == 'ё' || symbol == '-' || symbol == ' ') {
+        result = true;
+      } else {
+        result = false;
+        break;
+      }
+    }
+
+    StringTokenizer str = new StringTokenizer(fullName);
+    int wordsCounter = str.countTokens();
+    if (wordsCounter != 3) {
+      result = false;
+    }
+
+    return result;
+  }
+
+  public static String formattingFullName (String fullName) {
+    int space1 = fullName.indexOf(' ');
+    int space2 = fullName.lastIndexOf(' ');
+
+    String lastName = fullName.substring(0, space1).trim();
+    String firstName = fullName.substring(space1, space2).trim();
+    String patronymic = fullName.substring(space2).trim();
+
+    String template = "Фамилия: %s\nИмя: %s\nОтчество: %s";
+    return String.format(template, lastName, firstName,patronymic);
+  }
 }
