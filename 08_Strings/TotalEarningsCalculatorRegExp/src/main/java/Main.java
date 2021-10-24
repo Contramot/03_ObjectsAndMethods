5548e076412a7cccca8d7cef5f484256fc1d4fcb
Валерий Кравчук <valery.v.kravchuk@gmail.com>
2021-10-24 14:08:50 +0300

    Task TotalEarningsCalculatorRegExp completed

--- 08_Strings/TotalEarningsCalculatorRegExp/src/main/java/Main.java
+++ 08_Strings/TotalEarningsCalculatorRegExp/src/main/java/Main.java
@@ -1,12 +1,27 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
   public static void main(String[] args) {
-
-  }
+    String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
+    System.out.println(calculateSalarySum(text));
+    }
 
   public static int calculateSalarySum(String text){
-    //TODO: реализуйте метод
-    return -1;
+    // реализуйте метод
+    int salarySum = 0;
+    String regex = "[0-9]+";
+    Pattern pattern = Pattern.compile(regex);
+    Matcher matcher = pattern.matcher(text);
+    while (matcher.find()) {
+      int start = matcher.start();
+      int end = matcher.end();
+      String str = text.substring(start, end);
+      int number = Integer.parseInt(str);
+      salarySum += number;
+    }
+    return salarySum;
   }
 
 }
