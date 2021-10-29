88c730b396c5cd98e929c3cde3d1fe40837fdb21
Валерий Кравчук <valery.v.kravchuk@gmail.com>
2021-10-29 16:21:13 +0300

    Task ReverseArray started

--- 09_ArraysAndCollections/homework_9.1/ReverseArray/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.1/ReverseArray/src/main/java/Main.java
@@ -2,5 +2,14 @@ public class Main {
 
     public static void main(String[] args) {
         String line = "Каждый охотник желает знать, где сидит фазан";
+
+        String[] words = line.split(",?\\s+");
+
+        for (int i = 0; i < words.length; i++) {
+            System.out.println(words[i]);
+        }
+
+
+
     }
 }
--- 09_ArraysAndCollections/homework_9.1/ReverseArray/src/main/java/ReverseArray.java
+++ 09_ArraysAndCollections/homework_9.1/ReverseArray/src/main/java/ReverseArray.java
@@ -2,6 +2,10 @@ public class ReverseArray {
 
     //TODO: Напишите код, который меняет порядок расположения элементов внутри массива на обратный.
     public static String[] reverse (String[] strings){
+
+        for (int i = strings.length; i >= 0; i--) {
+
+        }
         return strings;
     }
 }
