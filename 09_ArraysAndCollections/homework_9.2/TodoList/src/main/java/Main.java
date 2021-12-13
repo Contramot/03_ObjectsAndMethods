f3d16197e5eefdc6e79517b2ded188343a14a138
Валерий Кравчук <valery.v.kravchuk@gmail.com>
2021-12-13 21:49:17 +0300

    Task TodoList started

--- 09_ArraysAndCollections/homework_9.2/TodoList/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.2/TodoList/src/main/java/Main.java
@@ -1,7 +1,21 @@
-public class Main {
-    private static TodoList todoList = new TodoList();
+import java.util.ArrayList;
+
+public class  Main {
+    //private static TodoList todoList = new TodoList();
 
     public static void main(String[] args) {
         // TODO: написать консольное приложение для работы со списком дел todoList
+
+        TodoList myTodoList = new TodoList();
+        myTodoList.add("Learn Java");
+        myTodoList.add("perekur");
+
+        myTodoList.add(4,"continue Learn Java");
+
+        myTodoList.edit("no smoking", 1);
+
+        //myTodoList.getTodos();
+
+        //System.out.println(myTodoList.getTodos().get(0));
     }
 }
--- 09_ArraysAndCollections/homework_9.2/TodoList/src/main/java/TodoList.java
+++ 09_ArraysAndCollections/homework_9.2/TodoList/src/main/java/TodoList.java
@@ -2,18 +2,42 @@ import java.util.ArrayList;
 
 public class TodoList {
 
+    ArrayList<String> todoList;
+
+    public TodoList() {
+        this.todoList = new ArrayList<>();
+    }
+
     public void add(String todo) {
-        // TODO: добавьте переданное дело в конец списка
+        // Добавляет переданное дело в конец списка
+        todoList.add(todo);
     }
 
     public void add(int index, String todo) {
-        // TODO: добавьте дело на указаный индекс,
-        //  проверьте возможность добавления
+        // Добавляет дело на указаный индекс,
+        //  проверяет возможность добавления
+
+        if (index <= todoList.size()) {     // TODO добавить проверку индекса на отрицательное значение
+            todoList.add(index, todo);
+        } else {
+            todoList.add(todo);
+        }
+
+//        for (int i = 0; i < todoList.size(); i++) {
+//            System.out.println("Task " + (i + 1) + " : " + todoList.get(i));
+//        }
     }
 
     public void edit(String todo, int index) {
         // TODO: заменить дело на index переданным todo индекс,
         //  проверьте возможность изменения
+
+        if (index < todoList.size()) {
+            String oldValue = todoList.get(index);
+            todoList.set(index, todo);
+            System.out.println("Дело \"" + oldValue + "\" заменено на \"" + todoList.get(index) + "\"");
+        }
+
     }
 
     public void delete(int index) {
