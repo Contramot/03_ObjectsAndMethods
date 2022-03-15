0dfd18f2e4bd42c8d16590d60a0ed496d2ee1709
Валерий Кравчук <valery.v.kravchuk@gmail.com>
2022-03-15 17:45:28 +0300

    Task CoolNumbers completed

--- 09_ArraysAndCollections/homework_9.5/CoolNumber/src/main/java/CoolNumbers.java
+++ 09_ArraysAndCollections/homework_9.5/CoolNumber/src/main/java/CoolNumbers.java
@@ -1,29 +1,78 @@
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.TreeSet;
+import java.util.*;
 
 public class CoolNumbers {
 
+    public static final String LETTERS = "АВЕКМНОРСТУХ";
+
     public static List<String> generateCoolNumbers() {
-        return Collections.emptyList();
+
+        List<String> coolNumbers = new ArrayList<>();
+
+        String[] carLetters = {"А","В","Е","К","М","Н","О","Р","С","Т","У","Х"};
+
+        for (String firstCarLetter : carLetters) {
+
+            for (int n = 0; n < 10; n++) {
+                String c = Integer.toString(n);
+                String numbers = (c + c + c);
+
+                for (String secondCarLetter : carLetters) {
+
+                    for (String thirdCarLetter : carLetters) {
+
+                        for (int r = 0; r < generateRegionCodes().size(); r++) {
+                            coolNumbers.add(firstCarLetter + numbers + secondCarLetter + thirdCarLetter + generateRegionCodes().get(r));
+                        }
+                    }
+                }
+            }
+        }
+
+        return coolNumbers;
+    }
+
+    public static List<String> generateRegionCodes() {
+
+        List<String> regionCodes = new ArrayList<>();
+
+        for (int i = 1; i < 10; i++) {
+            String c = Integer.toString(i);
+            String code = ("0" + c);
+            regionCodes.add(code);
+        }
+
+        for (int i = 10; i < 200; i++) {
+            String c = Integer.toString(i);
+            regionCodes.add(c);
+        }
+
+        return regionCodes;
     }
 
     public static boolean bruteForceSearchInList(List<String> list, String number) {
-        return false;
+
+        int index = -1;
+
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).equals(number)) {
+                index = i;
+                break;
+            }
+        }
+
+        return index >= 0;
     }
 
     public static boolean binarySearchInList(List<String> sortedList, String number) {
-        return false;
+        return Collections.binarySearch(sortedList, number) >= 0;
     }
 
-
     public static boolean searchInHashSet(HashSet<String> hashSet, String number) {
-        return false;
+        return hashSet.contains(number);
     }
 
     public static boolean searchInTreeSet(TreeSet<String> treeSet, String number) {
-        return false;
+        return treeSet.contains(number);
     }
 
 }
--- 09_ArraysAndCollections/homework_9.5/CoolNumber/src/main/java/Main.java
+++ 09_ArraysAndCollections/homework_9.5/CoolNumber/src/main/java/Main.java
@@ -1,12 +1,104 @@
+import java.util.*;
+
 public class Main {
     /*
-    TODO:
+
      - реализовать методы класса CoolNumbers
      - посчитать время поиска введимого номера в консоль в каждой из структуры данных
      - проанализоровать полученные данные
      */
 
     public static void main(String[] args) {
+
+        System.out.println("Введите номер для поиска:");
+
+        Scanner scanner = new Scanner(System.in);
+        String number = scanner.nextLine();
+
+        List<String> coolNumbers = CoolNumbers.generateCoolNumbers();
+        Collections.sort(coolNumbers);
+
+
+        long bruteForceSearchStart = System.nanoTime();
+
+        boolean bruteForceSearchResult = CoolNumbers.bruteForceSearchInList(coolNumbers, number);
+
+        long bruteForceSearchEnd = System.nanoTime();
+
+        long bruteForceSearchTime = bruteForceSearchEnd - bruteForceSearchStart;
+
+        System.out.println("Поиск перебором: " +
+                (bruteForceSearchResult ? "номер найден" : "номер не найден") +
+                ", поиск занял " + bruteForceSearchTime + " нс");
+
+
+        long binarySearchStart = System.nanoTime();
+
+        boolean binarySearchResult = CoolNumbers.binarySearchInList(coolNumbers, number);
+
+        long binarySearchEnd = System.nanoTime();
+
+        long binarySearchTime = binarySearchEnd - binarySearchStart;
+
+        System.out.println("Бинарный поиск: " +
+                (binarySearchResult ? "номер найден" : "номер не найден") +
+                ", поиск занял " + binarySearchTime + " нс");
+
+
+        HashSet<String> coolNumbersHashSet = new HashSet<>(coolNumbers);
+
+        long hashSetSearchStart = System.nanoTime();
+
+        boolean hashSetSearchResult = CoolNumbers.searchInHashSet(coolNumbersHashSet, number);
+
+        long hashSetSearchEnd = System.nanoTime();
+
+        long hashSetSearchTime = hashSetSearchEnd - hashSetSearchStart;
+
+        System.out.println("Поиск в HashSet: " +
+                (hashSetSearchResult ? "номер найден" : "номер не найден") +
+                ", поиск занял " + hashSetSearchTime + " нс");
+
+
+        TreeSet<String> coolNumbersTreeSet = new TreeSet<>(coolNumbers);
+
+        long treeSetSearchStart = System.nanoTime();
+
+        boolean treeSetSearchResult = CoolNumbers.searchInTreeSet(coolNumbersTreeSet, number);
+
+        long treeSetSearchEnd = System.nanoTime();
+
+        long treeSetSearchTime = treeSetSearchEnd - treeSetSearchStart;
+
+        System.out.println("Поиск в TreeSet: " +
+                (treeSetSearchResult ? "Номер найден" : "Номер не найден") +
+                ", поиск занял " + treeSetSearchTime + " нс");
+
+
+        List<Long> searchTime = Arrays.asList(bruteForceSearchTime, binarySearchTime, hashSetSearchTime, treeSetSearchTime);
+
+        List<String> searchMethod = Arrays.asList("Поиск перебором","Бинарный поиск","Поиск в HashSet","Поиск в TreeSet");
+
+        String minSearchMethod = searchMethod.get(0);
+        String maxSearchMethod = searchMethod.get(0);
+
+        long minSearchTime = searchTime.get(0);
+        long maxSearchTime = searchTime.get(0);
+
+        for (int i = 0; i < searchTime.size(); i++) {
+            if (searchTime.get(i) < minSearchTime) {
+                minSearchTime = searchTime.get(i);
+                minSearchMethod = searchMethod.get(i);
+            }
+            if (searchTime.get(i) > maxSearchTime) {
+                maxSearchTime = searchTime.get(i);
+                maxSearchMethod = searchMethod.get(i);
+            }
+        }
+
+        System.out.println("\nВ данной итерации:");
+        System.out.println("\tсамый быстрый поиск - " + minSearchMethod + ": " + minSearchTime + " нс.");
+        System.out.println("\tсамый медленный поиск - " + maxSearchMethod + ": " + maxSearchTime + " нс.");
 
     }
 }
